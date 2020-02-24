da5bcb22b99bb69ae1c49610285134849fbbf4dc
Nikendwi13 <nikendwirahayu@std.unissula.ac.id>
2020-02-24 14:53:33 +0700

    Update

---
+++ app/src/main/java/com/example/mynoteapp/NoteDao.kt
@@ -0,0 +1,21 @@
+package com.example.mynoteapp
+
+import android.icu.text.CaseMap
+import androidx.room.*
+import androidx.room.OnConflictStrategy.REPLACE
+
+@Dao
+interface  NoteDao{
+
+    @Query(value = "SELECT * from note")
+    fun getAllNote(): List<NoteModel>
+
+    @Insert(onConflict = REPLACE)
+    fun insertNote(note: NoteModel)
+
+    @Delete
+    fun deleteNote(note: NoteModel)
+
+    @Query(value = "UPDATE note SET title =:noteTitle, message =:noteMessage  WHERE id=:noteId")
+    fun updateNote(noteTitle:String, noteMessage:String, noteId:Long)
+}
